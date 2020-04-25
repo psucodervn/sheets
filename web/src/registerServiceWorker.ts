f83b84786687fa7beeb4dcdd50977a7923d1a5c4
Hưng Lê <hungle.info@gmail.com>
2020-04-25 21:34:34 +0700

    Add notify to update cached

--- web/src/registerServiceWorker.ts
+++ web/src/registerServiceWorker.ts
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker';
+import { Notify } from 'quasar';
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log(
         'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB',
+          'For more details, visit https://goo.gl/AFskqB'
       );
     },
     registered() {
@@ -21,9 +22,34 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated() {
       console.log('New content is available; please refresh.');
+      Notify.create({
+        color: 'negative',
+        icon: 'cached',
+        message: 'Updated content is available. Please refresh the page.',
+        timeout: 0,
+        multiLine: true,
+        position: 'top',
+        actions: [
+          {
+            label: 'Refresh',
+            color: 'yellow',
+            handler: () => {
+              window.location.reload();
+            },
+          },
+          {
+            label: 'Dismiss',
+            color: 'white',
+            handler: () => {
+            },
+          },
+        ],
+      });
     },
     offline() {
-      console.log('No internet connection found. App is running in offline mode.');
+      console.log(
+        'No internet connection found. App is running in offline mode.',
+      );
     },
     error(error) {
       console.error('Error during service worker registration:', error);
